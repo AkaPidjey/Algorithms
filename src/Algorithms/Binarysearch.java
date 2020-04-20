64d28068560cbe90f8cce3e60c7aefcd04054895
AkaPJ <pidjey@gmail.com>
2020-04-20 20:49:30 +0300

    Title: Binary search

---
+++ src/Algorithms/Binarysearch.java
@@ -0,0 +1,31 @@
+package Algorithms;
+
+public class Binarysearch {
+    /**
+     * Title: Binary search
+     * @author PJ
+     * @since 20/04/2020
+     * @version 1.0
+     * @param list - input array.
+     * @param item - input desired value.
+     * @return mid - number of the element in the array. Or 0 if there are no matches
+     */
+
+    public static int binary_search(int[] list, int item) {
+        int low = 0;
+        int high = list.length - 1;
+
+        while (low <= high) {
+            int mid = low + high;
+            int guess = list[mid];
+            if (guess == item) {
+                return mid;
+            } else if (guess > item) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/Algorithms/BinarysearchTest.java
@@ -0,0 +1,46 @@
+package Algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+
+public class BinarysearchTest {
+    @Test
+    public void whenValue5() {
+        int[] input = new int[100];
+        for (int i = 0; i < input.length; i++) {
+            input[i] = i + 1;
+        }
+        int value = 5;
+        int result = Binarysearch.binary_search(input, value);
+        int expect = 4;
+        assertEquals(result, expect);
+    }
+
+    @Test
+    public void whenValue100() {
+        int[] input = new int[100];
+        for (int i = 0; i < input.length; i++) {
+            input[i] = i + 1;
+        }
+        int value = 100;
+        int result = Binarysearch.binary_search(input, value);
+        int expect = 99;
+        assertEquals(result, expect);
+    }
+
+    @Test
+    public void whenValue0() {
+        int[] input = new int[100];
+        for (int i = 0; i < input.length; i++) {
+            input[i] = i + 1;
+        }
+        int value = 150;
+        int result = Binarysearch.binary_search(input, value);
+        int expect = 0;
+        assertEquals(result, expect);
+    }
+
+}
