c967122407de8cfd24d09992388ef33c7fb94bd7
AkaPJ <pidjey@gmail.com>
2020-05-13 14:54:50 +0300

    1 commit

---
+++ src/Algorithms/BubbleSortTest.java
@@ -0,0 +1,16 @@
+package Algorithms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BubbleSortTest {
+
+    @Test
+    public void descendingSort() {
+    }
+
+    @Test
+    public void ascendingSort() {
+    }
+}
